494ee00c3797877342eecfb393e9ac0f20f7c15e
HassanGG <hassan.g.ghnewa@gmail.com>
2023-07-30 19:13:17 +0100

    feat: Created render function to render blue screen

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,20 @@ pub async fn run() {
     let mut state = WGPUState::new(window).await;
 
     event_loop.run(move |event, _, control_flow| match event {
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+
+        Event::MainEventsCleared => {
+            state.window().request_redraw();
+        }
+
         Event::WindowEvent {
             ref event,
             window_id,
@@ -67,10 +81,10 @@ pub async fn run() {
                     WindowEvent::Resized(physical_size) => {
                         state.resize(*physical_size);
                     }
+
                     WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
                         state.resize(**new_inner_size);
                     }
-
                     _ => {}
                 }
             }
