448a08ee80bc32773847119b465732de276aadba
HassanGG <hassan.g.ghnewa@gmail.com>
2023-08-01 15:59:58 +0100

    feat: Changed vertices to be two dimensional

--- src/vertex.rs
+++ src/vertex.rs
@@ -1,25 +1,25 @@
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
-    position: [f32; 3],
+    position: [f32; 2],
     color: [f32; 3],
 }
 
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
+        position: [0.0, 0.5],
         color: [0.5, 0.0, 0.5],
     }, 
     Vertex {
-        position: [0.0, -0.5, 0.0],
+        position: [0.0, -0.5],
         color: [0.5, 0.0, 0.5],
     }, 
     Vertex {
-        position: [0.5, -0.5, 0.0],
+        position: [0.5, -0.5],
         color: [0.5, 0.0, 0.5],
     }, 
     Vertex {
-        position: [0.5, 0.5, 0.0],
+        position: [0.5, 0.5],
         color: [0.5, 0.0, 0.5],
     }, 
 ];
@@ -38,10 +38,10 @@ impl Vertex {
                 wgpu::VertexAttribute {
                     offset: 0,
                     shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
+                    format: wgpu::VertexFormat::Float32x2,
                 },
                 wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    offset: std::mem::size_of::<[f32; 2]>() as wgpu::BufferAddress,
                     shader_location: 1,
                     format: wgpu::VertexFormat::Float32x3,
                 },
